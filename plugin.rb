2369476372d2d4bc996817ab2168e933df87138c
Daniel Lewandowski <dan.w.lewandowski@gmail.com>
2018-12-11 19:33:43 +0000

    Update plugin.rb

--- plugin.rb
+++ plugin.rb
@@ -133,3 +133,5 @@ register_emoji "ufo1", "/plugins/boardpc-emoji/images/ufo1.gif"
 register_emoji "bpclogo", "/plugins/boardpc-emoji/images/bpclogo.png"
 register_emoji "fraz", "/plugins/boardpc-emoji/images/fraz.png"
 register_emoji "suzuki", "/plugins/boardpc-emoji/images/suzuki.png"
+register_emoji "warb", "/plugins/boardpc-emoji/images/war_b.png"
+
